2b207147d8cf83c26c02e9c5f7136f6ffe65ff37
Ivano Milo <ivanomilo@gmail.com>
2026-02-18 12:27:15 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col">
+        <div>
+          Hello to my beautiful wife
+
+        </div>
+        <img src='/kitty.jpeg'></img>
+      </div> 
+    </>
+  )
+}
+
+export default App
