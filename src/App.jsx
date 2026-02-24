e43befce45e0f9cd829df5de5d4b71b23e05003b
ivanomilo <ivanomilo@gmail.com>
2026-02-24 13:38:37 -0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import './App.css'
+import { TwoPanelSection } from './components/Section'
 
 function App() {
 
   return (
-    <>
-      <div className="flex flex-col">
-        <div>
-          Hello to my beautiful wife
-
+    <div className="w-full m-auto bg">
+      <div className="flex flex-col w-full m-auto justify-center">
+        <div className="flex m-auto"> Larissa & Ivano </div>
+        <div className="flex flex-row justify-between w-full">
+          <div className="flex flex-row m-auto">
+            <div className="m-2">Where & When</div>
+            <div className="m-2">Travel & Accomodations</div>
+            <div className="m-2">Fun Things To Do</div>
+            <div className="m-2">RSVP</div>
+          </div>
         </div>
-        <img src='/kitty.jpeg'></img>
+        <TwoPanelSection
+          leftContent={
+            <div>
+              Hello to my beautiful wife
+            </div>
+          }
+          rightContent={
+            <img className="w-fit" src='/kitty.jpeg'></img>
+          }
+
+          ></TwoPanelSection>
       </div> 
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Section.jsx
@@ -0,0 +1,10 @@
+export function TwoPanelSection({leftContent, rightContent, leftBigger}){ 
+    const leftProportion= leftBigger ? "md:w-[66%]" : "md:w-[33%]"
+    const rightProportion= leftBigger ? "md:w-[33%]" : "md:w-[66%]"
+    return (
+       <div className="flex flex-col md:flex-row w-full border border-amber-600" >
+           <div className={"w-full m-auto " +leftProportion}>{leftContent}</div> 
+           <div className={"w-full m-auto " +rightProportion}>{rightContent}</div>
+       </div>
+    )
+}
