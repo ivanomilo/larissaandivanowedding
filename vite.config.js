042d37980d36100949587ac785be9933bf28b38e
ivanomilo <ivanomilo@gmail.com>
2026-02-24 13:45:13 -0500

    static deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Set your desired base path here
   plugins: [
     tailwindcss(),
     react({
